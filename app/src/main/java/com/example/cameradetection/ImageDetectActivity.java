921f0198be6436770325386604a36731dc30b523
Bayramali1999 <50273925+Bayramali1999@users.noreply.github.com>
2023-04-17 10:43:31 +0500

    Initial commit

---
+++ app/src/main/java/com/example/cameradetection/ImageDetectActivity.java
@@ -0,0 +1,18 @@
+package com.example.cameradetection;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageDetectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_detect);
+    }
+}
---
+++ app/src/main/java/com/example/cameradetection/OnPictureTakenListener.java
@@ -0,0 +1,8 @@
+package com.example.cameradetection;
+
+import android.graphics.Bitmap;
+
+public interface OnPictureTakenListener {
+
+    void onTaken(Bitmap bitmap);
+}
